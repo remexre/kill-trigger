5658be125bd0ff8b5f034fb929ce0177946ed00a
Nathan Ringo <remexre@gmail.com>
2016-06-13 20:44:13 -0500

    Adds console.

--- kt-server/index.go
+++ kt-server/index.go
@@ -28,18 +28,39 @@ const index = `<!DOCTYPE html>
 					console.log(this.statusText);
 					allButtons(b => b.disabled = false);
 				});
-				xhr.open("POST", "/api/" + id + "/send");
+				xhr.open("POST", "https://kill-trigger.herokuapp.com/api/" + id + "/send");
 				xhr.send();
 			};
 			allButtons(b => b.addEventListener("click", clickListener));
 		</script>
 		<script>
-			const commandNames = {{ .commands }};
+			const leftPadder = row => {
+				const str = row[0].toString();
+				const num = row[1] + 1;
+				if(str.length > num) return str;
+				return new Array(num - str.length).join("0") + str;
+			};
+
+			const commands = {{ .commands }};
 			const ws = new WebSocket("wss://kill-trigger.herokuapp.com/socket");
 			ws.binaryType = "arraybuffer";
 			ws.addEventListener("message", event => {
-				console.log("message", event);
-				window.lastEventData = event.data;
+				const cmds = commands.filter(cmd => cmd.ID == Number.parseInt(event.data));
+				let name = "unknown";
+				if(cmds.length > 0) {
+					name = cmds[0].Name;
+				}
+				const now = new Date();
+				const line = [
+					[now.getFullYear(), 4],
+					[now.getMonth(),    2],
+					[now.getDate(),     2],
+				].map(leftPadder).join("-") + " " + [
+					[now.getHours(),   2],
+					[now.getMinutes(), 2],
+					[now.getSeconds(), 2],
+				].map(leftPadder).join(":") + "\t" + name + "(" + event.data + ")\n";
+				document.getElementById("console").textContent += line;
 			});
 		</script>
 	</body>
